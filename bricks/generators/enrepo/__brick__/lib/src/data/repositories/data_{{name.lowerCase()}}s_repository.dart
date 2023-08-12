30fa37a000a7fc503f9b3bfcdbbb1aa977a5391e
laevad <77134175+laevad@users.noreply.github.com>
2023-08-12 17:11:53 +0800

    0.1.3 - added functions

--- bricks/generators/enrepo/__brick__/lib/src/data/repositories/data_{{name.lowerCase()}}s_repository.dart
+++ bricks/generators/enrepo/__brick__/lib/src/data/repositories/data_{{name.lowerCase()}}s_repository.dart
@@ -1,5 +1,35 @@
 import '../../domain/repositories/{{name.lowerCase()}}s_repository.dart';
+import '../../domain/entities/{{name.lowerCase()}}.dart';
 
 class {{name.pascalCase()}}sRepositoryImpl implements {{name.pascalCase()}}sRepository {
-
+  {{#get}}
+  @override
+  Future<{{name.pascalCase()}}> get{{name.pascalCase()}}(String uid) async {
+    throw UnimplementedError();
+  }
+  {{/get}}
+  {{#getAll}}
+  @override
+  Future<List<{{name.pascalCase()}}>> getAll{{name.pascalCase()}}s() async {
+    throw UnimplementedError();
+  }
+  {{/getAll}}
+  {{#add}}
+  @override
+  Future<void> add{{name.pascalCase()}}({{name.pascalCase()}} {{name.lowerCase()}}) async {
+    throw UnimplementedError();
+  }
+  {{/add}}
+  {{#update}}
+  @override
+  Future<void> update{{name.pascalCase()}}({{name.pascalCase()}} {{name.lowerCase()}}) async {
+    throw UnimplementedError();
+  }
+  {{/update}}
+  {{#delete}}
+  @override
+  Future<void> delete{{name.pascalCase()}}({{name.pascalCase()}} {{name.lowerCase()}}) async {
+    throw UnimplementedError();
+  }
+  {{/delete}}
 }
