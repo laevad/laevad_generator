fb03eaec48421d535721fb4fc2b4cc63002f0542
laevad <77134175+laevad@users.noreply.github.com>
2023-08-12 10:51:53 +0800

    fixed the path

--- bricks/generators/entity/__brick__/lib/src/data/repositories/data_{{name.lowerCase()}}s_repository.dart
+++ bricks/generators/entity/__brick__/lib/src/data/repositories/data_{{name.lowerCase()}}s_repository.dart
@@ -1,5 +1,4 @@
-import '../domain/entities/{{name.lowerCase()}}.dart';
-import '../domain/repositories/{{name.lowerCase()}}s_repository.dart';
+import '../../domain/repositories/{{name.lowerCase()}}s_repository.dart';
 
 class {{name.pascalCase()}}sRepositoryImpl implements {{name.pascalCase()}}sRepository {
 
