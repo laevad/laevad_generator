8f6a44bddef33bbc2c0575ebb1d42546679a24c1
laevad <77134175+laevad@users.noreply.github.com>
2023-08-12 14:03:00 +0800

    user

---
+++ bricks/generators/enrepo/__brick__/lib/src/domain/entities/{{name.lowerCase()}}.dart
@@ -0,0 +1,17 @@
+class {{name.pascalCase()}} {
+  final String id;
+  final String name;
+
+  {{name.pascalCase()}}({this.id, this.name});
+
+  factory {{name.pascalCase()}}.fromJson(Map<String, dynamic> json) {
+    return {{name.pascalCase()}}(
+      id: json['id'],
+      name: json['name'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+      };
