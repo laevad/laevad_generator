cf134ae068e8bb2ba439dcd140264af44d2b84e7
laevad <77134175+laevad@users.noreply.github.com>
2023-08-17 08:37:51 +0800

    observer

---
+++ bricks/generators/observer/__brick__/lib/src/app/pages/{{name.snakeCase()}}/{{observer.snakeCase()}}_observer.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+
+class _{{observer.snakeCase()}}UseCaseObserver
+    extends Observer
+    // TODO: add usecase observer <UseCaseResponse>
+     {
+      @override
+      void onComplete() {
+    // TODO: implement onComplete
+      }
+    
+      @override
+      void onError(e) {
+    // TODO: implement onError
+      }
+    
+      @override
+      void onNext(response) {
+    // TODO: implement onNext
+      }
+  
+}
